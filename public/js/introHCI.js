5fab6fe8e603dc9b7c73430e80d8e60529b18e3e
rileyfroehlich <rileyfro2@gmail.com>
2021-02-11 10:54:19 -0800

    Turning in lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,5 +26,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var getURL = "http://localhost:3000/project/" + idNumber
+	$.get(getURL, callBack)
+
 	console.log("User clicked on project " + idNumber);
 }
+
+function callBack(data){
+	console.log(data)
+
+
+}
+
+function addProject(result) {
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	  '<img src="' + result['image'] + '" class="img">' +
+	  '<p>' + result['title'] + '</p>' +
+	  '<p><small>' + result['date'] +
+	  '</small></p></a>'; 
+  }
